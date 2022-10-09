959ea18661ad314543e2826e527f9bd65dcb8427
jonathan <bayart.jonathan@laposte.net>
2022-10-09 19:32:55 +0200

    ajout ingredients et mesure

--- app.js
+++ app.js
@@ -14,17 +14,34 @@ const fetchCocktail = async () => {
 const cocktailDisplay = async () => {
     await fetchCocktail();
 
-    cocktails.drinks.lenght = 12;
+    // cocktails.drinks.lenght = 12;
 
+    // let details = document.querySelector('#result');
     result.innerHTML = cocktails.drinks.map((cocktail) => 
         `
         <li>
             <h2>${cocktail.strDrink}</h2>
             <img src="${cocktail.strDrinkThumb}"></img>
             <p class="description">${cocktail.strInstructions}</p>
+            <p class="mesureIngredient">${cocktail.strIngredient1} : ${cocktail.strMeasure1}</p>
+            <p class="mesureIngredient">${cocktail.strIngredient2} : ${cocktail.strMeasure2}</p>
+            <p class="mesureIngredient">${cocktail.strIngredient3} : ${cocktail.strMeasure3}</p>
+            <p class="mesureIngredient">${cocktail.strIngredient4} : ${cocktail.strMeasure4}</p>
+
         </li>
         `
     ).join("");
+
+    // for(let i=1; i<=15; i++){
+
+    //     if(cocktails.drinks[0][`strIngredient${i}`] == null){
+    //             break;
+    //         }
+    //     let ingredient = document.createElement('li');
+    //     ingredient.innerHTML = cocktails.drinks[0][`strIngredient${i}`] + ' : ' + cocktails.drinks[0][`strMeasure${i}`];
+
+    //     details.appendChild(ingredient);
+    // }
 };
 
 form.addEventListener("submit", (e) => {
